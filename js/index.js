fe003dc3478a1d233bac5b01fea1e7e6bb546e15
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-02-26 23:23:23 +0300

    ft:css files - add styling files for about me,projects and contact me pages

---
+++ js/index.js
@@ -0,0 +1,55 @@
+var btn1 = document.getElementById("vbtn");
+var btn1 = document.getElementById("vbtn");
+var btn1 = document.getElementById("vbtn");
+var btn1 = document.getElementById("vbtn");
+var btn5 = document.getElementById("vbtn");
+
+
+
+
+//Home Button 
+function nextPage()
+{
+    window.location("../about.html")
+}
+
+btn1.addEventListener("click", nextPage())
+
+
+//About Button 
+function nextPage()
+{
+    window.location("../about.html")
+}
+
+btn1.addEventListener("click", nextPage())
+
+
+
+//Projects Button 
+function nextPage()
+{
+    window.location("../about.html")
+}
+
+btn1.addEventListener("click", nextPage())
+
+
+
+//Home Button 
+function nextPage()
+{
+    window.location("../about.html")
+}
+
+btn1.addEventListener("click", nextPage())
+
+
+
+// View More button 
+function nextPage()
+{
+    window.location("../about.html")
+}
+
+btn5.addEventListener("click", nextPage())
