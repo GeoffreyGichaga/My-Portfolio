e5a7a4e8a93c2f4412b5dd8fd9dfde8425ee0553
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-02-28 04:07:22 +0300

    ft:form - Add a form on the contacts page

---
+++ js/contact.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById("sendbtn");
+
+btn.addEventListener("click", showAlert);
+
+function showAlert()
+{
+    alert("Thanks for reaching me ,I'll be intouch with you in a short while")
+
+    
+}
